d0125a03f0a2e02453daf4478ab9786bb49b817f
AngelinaR23 <130458806+AngelinaR23@users.noreply.github.com>
2023-04-27 11:21:08 +0700

    upload tugas

--- script.js
+++ script.js
@@ -11,38 +11,4 @@ function loadData() {
 
 loadData();
 
-fetch('https://pokeapi.co/api/v2/pokemon')
-    .then(function(response) {
-        if (response.status != 200) {
-            console.log('Error: ' + response.status);
-            return;
-        }
-        response.json().then(function(data) {
-            const pokemons = data.results;
-            pokemons.forEach(pokemon => {
-                document.getElementById('pokemonList')
-                    .insertAdjacentHTML('beforeend', `<li onclick='detail("${pokemon.url}")'>${pokemon.name}</li>`);
-            });
-        });
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
 
-function detail(url) {
-    fetch(url)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(pokemon) {
-            document.getElementById('detail').innerHTML = `
-                <h3>${pokemon.name}</h3>
-                <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-                <p>Weight: ${pokemon.weight}kg</p>
-                <p>Height: ${pokemon.height}m</p>
-            `;
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-}
